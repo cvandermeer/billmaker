d055360cf7ed572ef22e0d6e6d0129b081079d7a
Chris van der Meer <cvandermeer@live.nl>
2015-08-17 17:29:30 +0200

    renaming variable for model test

--- test/models/competence_test.rb
+++ test/models/competence_test.rb
@@ -4,7 +4,7 @@ class CompetenceTest < ActiveSupport::TestCase
   setup :initialize_competence
 
   def teardown
-    @competence1 = nil
+    @competence = nil
   end
 
   test 'should not create competence without any input' do
@@ -13,28 +13,28 @@ class CompetenceTest < ActiveSupport::TestCase
   end
 
   test 'should not create competence without title_id' do
-    competence = Competence.new(points: @competence1.points, level_id: @competence1.level_id, bill_id: @competence1.bill_id)
+    competence = Competence.new(points: @competence.points, level_id: @competence.level_id, bill_id: @competence.bill_id)
     assert_not competence.save, 'Saved a competence without title_id!'
   end
 
   test 'should not create competence without points' do
-    competence = Competence.new(title_id: @competence1.title_id, level_id: @competence1.level_id, bill_id: @competence1.bill_id)
+    competence = Competence.new(title_id: @competence.title_id, level_id: @competence.level_id, bill_id: @competence.bill_id)
     assert_not competence.save, 'Saved a competence without title_id!'
   end
 
   test 'should not create competence without level_id' do
-    competence = Competence.new(title_id: @competence1.title_id, points: @competence1.points, bill_id: @competence1.bill_id)
+    competence = Competence.new(title_id: @competence.title_id, points: @competence.points, bill_id: @competence.bill_id)
     assert_not competence.save, 'Saved a competence without title_id!'
   end
 
   test 'should not create competence without bill_id' do
-    competence = Competence.new(title_id: @competence1.title_id, points: @competence1.points, level_id: @competence1.level_id)
+    competence = Competence.new(title_id: @competence.title_id, points: @competence.points, level_id: @competence.level_id)
     assert_not competence.save, 'Saved a competence without title_id!'
   end
 
   private
 
   def initialize_competence
-    @competence1 = competences(:competence1)
+    @competence = competences(:competence1)
   end
 end
